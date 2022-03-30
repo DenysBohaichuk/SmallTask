c408bf176cbcaee7efddc1543f7de52cafcffd1d
Denys <denic.bohaichul.10@gmail.com>
2022-03-31 01:42:06 +0300

    SmallTask

---
+++ MvcMovie/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index(string sendButton, string UserName)
+        {
+            if (sendButton!= null && UserName != null)
+            {
+                ViewData["Message"] = UserName;
+                return View("HelloUserPage");
+            }
+            else if(sendButton != null && UserName == null)
+            {
+                return View("HelloAnonymousPage");
+            }
+            return View();
+        }
+
+    }
+}
